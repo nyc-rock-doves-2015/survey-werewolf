8f44a18ec9e6a7d3b774d0a6d79d04b9cba41ea1
Carl Swanson <cetswanson@gmail.com>
2015-04-17 15:01:24 -0400

    Add user's surveys route to surveys controller

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -6,6 +6,12 @@ get '/surveys/create' do
   erb :'/make-survey/make-survey'
 end
 
+get '/surveys/user/:id' do |id|
+  @user = User.find(id)
+  @surveys = @user.surveys
+  "List of #{@user.name}'s surveys"
+end
+
 #If owner: edit/delete/view results
 #If logged in: take/view results if already taken
 #If not logged in: redirect to signup/signin
