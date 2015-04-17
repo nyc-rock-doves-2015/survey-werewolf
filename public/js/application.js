a151056425a81643e2ed04cb8b9f19404124cc9d
Lucian Kahn <luciankahn@gmail.com>
2015-04-17 17:03:02 -0400

    remove checkboxes option

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // add a question to Make Survey form when user clicks on "add question"
   $('#add-question-field').on('click', function(event) {
     event.preventDefault();
-    var questionNode = "<p><label>Question</label></p><p><input type='text' name='question'></p><p><label>Answer Type</label></p><p><select class='select-answer-type'><option value='radio'>Choose One</option><option value='checkbox'>Choose Many</option></select></p>";
+    var questionNode = "<p><label>Question</label></p><p><input type='text' name='question'></p>";
     $('.question-container').append(questionNode);
   });
 });
