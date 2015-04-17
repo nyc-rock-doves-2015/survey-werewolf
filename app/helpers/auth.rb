5b2e44d1808accde5232826677352e8f19e93cfd
Charles Green <Charles.Green88@gmail.com>
2015-04-17 14:46:24 -0400

    added user auth controller with views

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -5,3 +5,21 @@ def current_user
     return nil
   end
 end
+
+def privacy_guard(survey)
+  if !survey.public && !survey_owner?(survey)
+    # flash[:error] = "Sorry, you do not have permission to see that."
+    redirect '/'
+  end
+end
+
+def survey_owner?(survey)
+  current_user && survey.user_id == current_user.id
+end
+
+def empty_sign_up_field?(user_params)
+  user_params.each_value do |param|
+    return true if param == ""
+  end
+  return false
+end
