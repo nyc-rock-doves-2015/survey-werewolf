a10effc98aebe53d9e1853620ba612102cbbb655
Lucian Kahn <luciankahn@gmail.com>
2015-04-17 16:58:56 -0400

    form field dynamically generated

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
   // add a question to Make Survey form when user clicks on "add question"
-  $('#add-question').on('click', function() {
-    $questionNode = "<p>
-                    <label>Question</label>
-                    </p>
-                    <p>
-                    <input type='text' name='question'>
-                    </p>
-
-                    <p>
-                    <label>Answer Type</label>
-                    </p>
-                    <p>
-                      <select class='select-answer-type'>
-                      <option value='radio'>Choose One</option>
-                      <option value='checkbox'>Choose Many</option>
-                    </select>
-                    </p>";
-    $(this).parent().append($questionNode);
+  $('#add-question-field').on('click', function(event) {
+    event.preventDefault();
+    var questionNode = "<p><label>Question</label></p><p><input type='text' name='question'></p><p><label>Answer Type</label></p><p><select class='select-answer-type'><option value='radio'>Choose One</option><option value='checkbox'>Choose Many</option></select></p>";
+    $('.question-container').append(questionNode);
   });
 });
