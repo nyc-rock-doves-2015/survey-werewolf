cc3143f30811f58f689e21b8ab6032f1f28df8d7
Lucian Kahn <luciankahn@gmail.com>
2015-04-17 17:10:18 -0400

    dynamic form contains answer options

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
   // add a question to Make Survey form when user clicks on "add question"
   $('#add-question-field').on('click', function(event) {
     event.preventDefault();
-    var questionNode = "<p><label>Question</label></p><p><input type='text' name='question'></p>";
+    var questionNode = "<p><label>Question</label></p><p><input type='text' name='question'></p><p><span id='add-answer-option-field'>Add Answer Option</span></p><div class='answer-option-container'></div>";
     $('.question-container').append(questionNode);
+
+    $('#add-answer-option-field').on('click', function(event) {
+      var answerOptionNode = "<p><label>Answer Option: </label> <input type='text' name='answer-option'></p>";
+      $('.answer-option-container').append(answerOptionNode);
+    });
   });
 });
