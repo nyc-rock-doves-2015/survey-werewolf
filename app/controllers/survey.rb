2593f75a7c28c2562753d3253435e73bf73d182c
Carl Swanson <cetswanson@gmail.com>
2015-04-17 14:31:14 -0400

    Update survey routes

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,14 +1,15 @@
-get '/' do
-  "Index Page"
-end
-
 get '/surveys' do
   "List of surveys"
+end
+
+get '/surveys/create' do
+  erb :'/make-survey/make-survey'
 end
 
 #If owner: edit/delete/view results
 #If logged in: take/view results if already taken
 #If not logged in: redirect to signup/signin
 get '/surveys/:id' do |id|
-  "Individual survey"
+  erb :'/take-survey/take-survey'
 end
+
