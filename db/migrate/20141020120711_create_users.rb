f0fbc4956bc37011f57b3942b56b5dfacb10426b
Carl Swanson <cetswanson@gmail.com>
2015-04-18 18:35:09 -0400

    Add assocation from user table to response table

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :password_digest
+      t.references :response
+
+      t.timestamps
     end
   end
 end
