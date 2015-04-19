f1f058d176d54d12b4e17664062cbb0808c22c7a
Carl Swanson <cetswanson@gmail.com>
2015-04-19 15:14:11 -0400

    Remove survey results route from survey controller

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -30,11 +30,6 @@ get '/surveys/user/:id' do |id|
   "List of #{@user.name}'s surveys"
 end
 
-get '/surveys/:id/results' do |id|
-  @survey = Survey.find(id)
-  "Results of #{@survey.title}"
-end
-
 #If owner: edit/delete/view results
 #If logged in: take/view results if already taken
 #If not logged in: redirect to signup/signin
