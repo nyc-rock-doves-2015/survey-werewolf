907577d8c00aff51471b31c83fcb67d223b060d1
Carl Swanson <cetswanson@gmail.com>
2015-04-19 14:36:09 -0400

    Add reference to survey in user migration

--- db/migrate/20141020120711_create_users.rb
+++ db/migrate/20141020120711_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :password_digest
       t.references :response
+      t.references :survey
 
       t.timestamps
     end
