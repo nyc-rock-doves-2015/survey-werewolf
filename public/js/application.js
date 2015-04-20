25a36c429e72aa2e69491208f6aafb7c334ef011
Charles Green <Charles.Green88@gmail.com>
2015-04-19 22:06:06 -0400

    fixed layout file and footer positioning

--- public/js/application.js
+++ public/js/application.js
@@ -19,7 +19,32 @@ $(document).ready(function() {
     });
   });
   
-  $('.auto-focus:first').focus();
+  $('.auto-focus:last').focus();
+
+  $(document).ready(function() {
+  var element = document.getElementById("js-fadeInElement");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    if( $("#js-fadeInElement").length > 0 ) {
+      var elementTopToPageTop = $(element).offset().top;
+      var windowTopToPageTop = $(window).scrollTop();
+      var windowInnerHeight = window.innerHeight;
+      var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+      var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+      var distanceFromBottomToAppear = 100;
+
+      if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+        $(element).addClass('js-fade-element-show');
+      }
+      else if(elementTopToWindowBottom < 0) {
+        $(element).removeClass('js-fade-element-show');
+        $(element).addClass('js-fade-element-hide');
+      }
+    }
+  });
+});
+
 
   // // error message for empty search results
   // $('#search').on('submit', function(e) {
