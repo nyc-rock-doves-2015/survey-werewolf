cb01c97fa22062bcd1c106c5d3fe83020e5abb0c
Charles Green <Charles.Green88@gmail.com>
2015-04-19 20:03:37 -0400

    default seed images working. Added dynamic main page with create survey button hidden from guests

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'faker'
 10.times do
   usertest = User.create(name: Faker::Name.name, password: "password")
   5.times do
-    surveytest = usertest.surveys.create(title: Faker::Commerce.department, image: "https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png")
+    surveytest = usertest.surveys.create(title: Faker::Commerce.department, image: open("https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png"))
     4.times do
       questiontest = surveytest.questions.create(question_desc: Faker::Lorem.word)
       3.times do
